19937953d1a8c9c1aa5b2abd658546ffd32398a0
RahulBayad <rahulbayad05@gmail.com>
2025-08-16 21:31:14 +0530

    Project initialised

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Link href="/about">About</Link>
+    </div>
+  );
+}
